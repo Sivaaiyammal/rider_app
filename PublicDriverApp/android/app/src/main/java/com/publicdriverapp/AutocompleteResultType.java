ae73e8c6210d09dbc70b86ead77d6f429b28d27e
Raja Palani <rajap@virtualmaze.co.in>
2024-09-05 11:22:56 +0530

    NOT Driver APP setup and Map Initilize added

---
+++ PublicDriverApp/android/app/src/main/java/com/publicdriverapp/AutocompleteResultType.java
@@ -0,0 +1,10 @@
+package com.publicdriverapp;
+
+public enum AutocompleteResultType {
+    TYPE_LOCATIONS,
+    TYPE_STREET,
+    TYPE_ADDRESS,
+    TYPE_POI,
+    TYPE_ACTIONS,
+    TYPE_UN_CATEGORY
+}
