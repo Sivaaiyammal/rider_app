7b72548f76058f2bbe48178f574a1ddc9795a9e6
hari <hari@haris-Laptop.local>
2025-08-28 17:32:59 +0530

    DElETE:driverApp

--- PublicDriverApp/src/store/useDriverStatusStore.js
+++
@@ -1,8 +0,0 @@
-import {create} from 'zustand';
-
-const useDriverStatusStore = create(set => ({
-  driverStatus: 'Online',
-  setDriverStatus: driverStatus => set({driverStatus}),
-}));
-
-export default useDriverStatusStore;
