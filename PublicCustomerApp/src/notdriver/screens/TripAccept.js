7f52b5f3b78b158c106b166a8937eef519d51e7e
hari <hari@haris-Laptop.local>
2026-01-13 12:16:21 +0530

    Fix:driver socker

--- PublicCustomerApp/src/notdriver/screens/TripAccept.js
+++ PublicCustomerApp/src/notdriver/screens/TripAccept.js
@@ -253,6 +253,7 @@ const TripAccept = () => {
           logout('driver');
            BGLocationTask.stopDriverBgTask();
            tripAlert.stopAlertSound();
+           RideMatchWSService.close();
           return
         }
      } catch (err) {
